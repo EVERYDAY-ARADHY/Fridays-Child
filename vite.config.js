a38feae042bf10e5c8a09567a4ac62814a59258b
Aradhy Shukla <krishnaxaradhy@gmail.com>
2026-06-28 18:29:48 +0530

    Fix Vercel Vite build issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        creation: resolve(__dirname, 'creation.html'),
+        about: resolve(__dirname, 'about.html')
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true,
